73e71e7026401c0633fdd5e6ad029a45bccb43fd
Mitchell Abrams <mitchell.abrams@duke.edu>
2022-07-01 11:36:43 -0400

    Prepare for 1.2.0 release

--- fars_cleaner/__init__.py
+++ fars_cleaner/__init__.py
@@ -1,4 +1,6 @@
 name = 'fars_cleaner'
+__version__ = "1.2.0"
+
 from fars_cleaner.datasets import FARSFetcher
 from fars_cleaner.fars_processor import FARSProcessor
 from fars_cleaner.data_loader import load_pipeline
