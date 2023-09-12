e3565b7c35af985bf79f41e11db97efbd1ffa731
Mitchell Abrams <mitchell@eduroam-172-16-104-206.trin.private.cam.ac.uk>
2023-09-12 11:35:31 +0100

    Update unit tests for 2021 data

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -58,7 +58,7 @@ def test_fetch_all():
     fetcher = FARSFetcher()
     result = fetcher.fetch_all()
     from pathlib import Path
-    for year in range(1975, 2021):
+    for year in range(1975, 2022):
         zip_path = fetcher.get_data_path() / f"{year}.zip"
         unzip_path = fetcher.get_data_path() / f"{year}.unzip"
         accident_path = "ACCIDENT.CSV"
